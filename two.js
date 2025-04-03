97cd51de0df4673b92931050e48a76b68b7dc64d
Alina-skypro2025 <alin.guschina2018@yandex.ru>
2025-04-03 22:58:07 +0500

    добавлена 2-я игра

---
+++ two.js
@@ -0,0 +1,43 @@
+
+function generateTask() {
+    const num1 = Math.floor(Math.random() * 20) + 1;
+    const num2 = Math.floor(Math.random() * 20) + 1;
+    const operators = ['+', '-', '*', '/'];
+    const operator = operators[Math.floor(Math.random() * operators.length)];
+
+
+    let task = '';
+    let correctAnswer = 0;
+
+    switch (operator) {
+        case '+':
+            task = `${num1} + ${num2}`;
+            correctAnswer = num1 + num2;
+            break;
+        case '-':
+            task = `${num1} - ${num2}`;
+            correctAnswer = num1 - num2;
+            break;
+        case '*':
+            task = `${num1} * ${num2}`;
+            correctAnswer = num1 * num2;
+            break;
+        case '/':
+            task = `${num1 * num2} / ${num2}`;
+            correctAnswer = num1;
+            break;
+    }
+    return { task, correctAnswer };
+}
+
+const { task, correctAnswer } = generateTask();
+
+
+const userAnswer = prompt(`Решите задачу: ${task}`);
+
+
+if (parseFloat(userAnswer) === correctAnswer) {
+    alert("Ответ верный!");
+} else {
+    alert(`Неверно. Правильный ответ: ${correctAnswer}`);
+}
