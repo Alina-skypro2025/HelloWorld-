822c9e9d1fea614b1c64ccda1e6f05dbc315b210
Alina-skypro2025 <alin.guschina2018@yandex.ru>
2025-04-25 21:16:56 +0500

    6 игра

--- six.js
+++ six.js
@@ -6,6 +6,5 @@ document.getElementById('colorButton').addEventListener('click', function() {
 
     const toHex = (n) => n.toString(16).padStart(2, '0');
     const randomColor = `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-    
     document.body.style.backgroundColor = randomColor;
 });
